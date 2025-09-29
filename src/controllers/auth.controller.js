c4dd2dceecfc39f1893c068f4d58a65841d434dc
¨lautaro <shortlauty35@gmail.com>
2025-09-29 10:43:38 -0300

    se realizaron los controladores de registro y login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,18 @@
+import { comparePassword, hashPassword } from "../helpers/bcrypt.helper.js";
+import { signToken } from "../helpers/jwt.helper.js";
+import { UserModel } from "../models/mongoose/user.model.js";
+
 export const register = async (req, res) => {
+  const { username, email, password, role, profile } = req.body;
   try {
+    const hasedpassword = await hashPassword(password);
+    await UserModel.create({
+      username: username,
+      email: email,
+      role: role,
+      password: hasedpassword,
+      profile: profile,
+    });
     // TODO: crear usuario con password hasheada y profile embebido
     return res.status(201).json({ msg: "Usuario registrado correctamente" });
   } catch (error) {
@@ -9,7 +22,28 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
+  const { username, password } = req.body;
   try {
+    const user = await UserModel.findOne({ username: username });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ msg: "el usuario o la contraseña es incorrecta" });
+    }
+    const correctPassword = await comparePassword(password, user.password);
+    if (!correctPassword) {
+      return res
+        .status(400)
+        .json({ msg: "el usuario o la contraseña es incorrecta" });
+    }
+    const token = signToken({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+      profile: user.profile,
+    });
+    res.cookie("token", token, { httpOnly: true, maxAge: 1000 * 60 * 60 });
     // TODO: buscar user, validar password, firmar JWT y setear cookie httpOnly
     return res.status(200).json({ msg: "Usuario logueado correctamente" });
   } catch (error) {
@@ -20,8 +54,9 @@ export const login = async (req, res) => {
 
 export const getProfile = async (req, res) => {
   try {
+    // const user = req.user;
     // TODO: devolver profile del user logueado actualmente
-    return res.status(200).json({ data: profile });
+    return res.status(200).json({ data: user });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ msg: "Error interno del servidor" });
